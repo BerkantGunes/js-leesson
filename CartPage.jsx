6e98262b9f3cb2e38f58a5ec45951391977ee5e5
Berkant Gunes <gunesberkant96@gmail.com>
2026-01-07 19:50:33 +0300

    Add files via upload

--- CartPage.jsx
+++ CartPage.jsx
@@ -99,7 +99,15 @@ const CartPage = () => {
     <>
       <Header />
       <div className="px-6">
-        <Table dataSource={cart.cartItems} columns={columns} bordered pagination={false} />
+        <Table
+         dataSource={cart.cartItems} 
+         columns={columns} 
+         bordered pagination={false} 
+         scroll={{
+          x: 1200,
+          y: 300
+         }} 
+        />
         <div className="cart-total flex justify-end mt-4">
           <Card className="w-72">
             <div className="flex justify-between">
